9567ba5bf6e47c9c8015426d342b2f27ff7dda1b
Mårten Söderberg <marten@martensoderberg.se>
2017-12-07 01:01:08 +0100

    shitty first broken attempt

--- 2017/1/captcha.py
+++ 2017/1/captcha.py
@@ -1,1 +1,15 @@
+import sys
 
+inputFile = open("input.txt", "r")
+line = inputFile.readline()
+
+max = len(line)
+sum = 0
+for i in range(0, max):
+  thisChar = line[i]
+  nextChar = line[(i + 1) % max] # loop-around when nextChar is the last
+  if thisChar == nextChar:
+    print(thisChar)
+    sum += int(thisChar)
+
+print(str(sum))
