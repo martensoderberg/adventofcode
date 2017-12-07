48deae51fcec5b240e3011dc09cab82a19c300be
Mårten Söderberg <marten@martensoderberg.se>
2017-12-07 21:04:36 +0100

    functionized

--- 2017/1/captcha.py
+++ 2017/1/captcha.py
@@ -1,15 +1,17 @@
 import sys
 
-inputFile = open("input.txt", "r")
-line = inputFile.readline()
+def calculateDuplicatesSum(line):
+  max = len(line)
+  sum = 0
+  for i in range(0, max):
+    thisChar = line[i]
+    nextChar = line[(i + 1) % max] # loop-around when nextChar is the last
+    if thisChar == nextChar:
+      sum += int(thisChar)
+  return sum
 
-max = len(line)
-sum = 0
-for i in range(0, max):
-  thisChar = line[i]
-  nextChar = line[(i + 1) % max] # loop-around when nextChar is the last
-  if thisChar == nextChar:
-    print(thisChar)
-    sum += int(thisChar)
-
-print(str(sum))
+if __name__ == "__main__":
+  inputFile = open("input.txt", "r")
+  line = inputFile.readline()
+  sum = calculateDuplicatesSum(line)
+  print(str(sum))
